deb4f55de7c31f3b7b5f3b24a46e1604259602c0
d50433698 <73023847+d50433698@users.noreply.github.com>
2021-07-20 11:41:54 +0300

    Update UserEdit.jsx

--- react/src/pages/UserEdit.jsx
+++ react/src/pages/UserEdit.jsx
@@ -1,45 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import UserService from '../services/userService';
 import { useParams } from 'react-router';
-import { Button, Card, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 export default function UserDetail() {
   let { id } = useParams();
   const [user, setUser] = useState([]);
-
+ 
+   
+ 
  
 
-  function handleSubmit(e){
+  function handleSubmit(event){
+    event.preventDefault();
+    const data = new FormData(event.target);
     let userService = new UserService();
-    userService.addUser(e).then(response => console.log(response));
+    userService.addUser(data).then(response => console.log(response));
   }
 
   useEffect(() => {
+    handleSubmit = handleSubmit.bind(this);
     let userService = new UserService();
     userService.getUserById(id).then(response =>
-      // console.log(response.data)
       setUser(response.data)
     );
+    console.log(user);
   }, []);
   return (
     <div>
       User
         <div>
-          <form onSubmit={handleSubmit(e)}>
+          <form onSubmit={handleSubmit}>
             {' '}
             <div>
               <label>User Name: </label>
+              <input type="text" id="name" placeholder={user.name} name="name" />{' '}
+            </div>
+            <div>
+              <label>User Phone: </label>
+               <input type="text" id="phone" name="phone" />{' '}
+            </div>
+            <div>
+              <label>User Adress: </label> 
               <input type="text" />{' '}
             </div>
             <div>
-              <label>User Phone: </label> <input type="text" />{' '}
+              <label>Company Name: </label> 
+              <input type="text" id="company" name="company"/>{' '}
             </div>
             <div>
-              <label>User Adress: </label> <input type="text" />{' '}
-            </div>
-            <div>
-              <label>Company Name: </label> <input type="text" />{' '}
+              <label>Email: </label> 
+              <input type="text" id="email" name="email"/>{' '}
             </div>
             <Link to={{ pathname: `/` }}>
               <button type="button" class="btn btn-info">
